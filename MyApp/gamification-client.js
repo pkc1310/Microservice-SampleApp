e54c00ac53b8c9833cb9ad3b6143d44e55958cbe
Pankaj Chouhan <pkc1310@gmail.com>
2021-02-20 15:53:21 +0530

    Adding UI

---
+++ MyApp/gamification-client.js
@@ -0,0 +1,36 @@
+function updateLeaderBoard() {
+ $.ajax({
+ url: "http://localhost:9001/api/leaders"
+ }).then(function(data) {
+ $('#leaderboard-body').empty();
+ data.forEach(function(row) {
+ $('#leaderboard-body').append('<tr><td>' + row.
+userId + '</td>' +
+'<td>' + row.totalScore + '</td>');
+ });
+ });
+}
+function updateStats(userId) {
+ $.ajax({
+ url: "http://localhost:9001/api/stats?userId=" + userId,
+ success: function(data) {
+ $('#stats-div').show();
+ $('#stats-user-id').empty().append(userId);
+ $('#stats-score').empty().append(data.score);
+ $('#stats-badges').empty().append(data.badges.
+join());
+ },
+ error: function(data) {
+ $('#stats-div').show();
+ $('#stats-user-id').empty().append(userId);
+ $('#stats-score').empty().append(0);
+ $('#stats-badges').empty();
+ }
+ });
+}
+$(document).ready(function() {
+ updateLeaderBoard();
+ $("#refresh-leaderboard").click(function( event ) {
+ updateLeaderBoard();
+ });
+});
